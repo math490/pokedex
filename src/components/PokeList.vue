86bf345b3695b0959cb96741673864e30055496a
math490 <telesm294@gmail.com>
2025-07-12 16:01:36 -0400

    Feat: começo do projeto

--- src/components/PokeList.vue
+++ src/components/PokeList.vue
@@ -1,12 +1,13 @@
 <template>
+  <link href="https://db.onlinewebfonts.com/c/073a3b73a63a87e100f6bb8f003fc0d3?family=Flexo-Demi" rel="stylesheet">
   <div class="container">
     <div
       class="container-item"
       v-for="pokemon in pokemons"
       :key="pokemon.name"
     >
-      <v-card>
-        <v-card-title>{{ pokemon.name }}</v-card-title>
+      <v-card color="purple" min-width="150" max-width="300" height="220" class="cartoes">
+        <v-card-title>{{ pokemon.name.toUpperCase() }}</v-card-title>
         <v-img :src="pokemon.image" :width="150"></v-img>
       </v-card>
     </div>
@@ -18,7 +19,8 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   props: ['pokemons']
-});
+})
+
 </script>
 
 <style scoped>
@@ -30,4 +32,21 @@ export default defineComponent({
 .container-item {
   margin: 15px;
 }
+
+.cartoes {
+  font-family: "Flexo-Demi";
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  white-space: nowrap; /* Impede que o texto quebre em várias linhas */
+  overflow: hidden; /* Esconde o texto que excede a largura */
+  text-overflow: ellipsis; /* Adiciona "..." no final do texto cortado */
+}
+
+.cartoes.overflow {
+  font-size: 14px; /* Tamanho da fonte quando há estouro */
+}
+
 </style>
